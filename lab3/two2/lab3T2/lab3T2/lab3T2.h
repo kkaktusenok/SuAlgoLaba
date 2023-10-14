1f50592a0e978ed207838c516fbbd0a589f299d4
kkaktusenok <banemen2003@gmail.com>
2023-10-14 13:32:06 +0600

    main

---
+++ lab3/two2/lab3T2/lab3T2/lab3T2.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+class DynamicStack {
+public:
+    int data;
+    DynamicStack* next;
+
+    DynamicStack(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedStack {
+public:
+    LinkedStack() : top_(nullptr), size_(0) {}
+
+    void push(int value) {
+        DynamicStack* newNode = new DynamicStack(value);
+        newNode->next = top_;
+        top_ = newNode;
+        size_++;
+    }
+
+    void pop() {
+        if (!isEmpty()) {
+            DynamicStack* temp = top_;
+            top_ = top_->next;
+            delete temp;
+            size_--;
+        } else {
+            cerr << "Stack is empty. Cannot pop." << endl;
+        }
+    }
+
+    int top() const {
+        if (!isEmpty()) {
+            return top_->data;
+        }
+        throw out_of_range("Stack is empty");
+    }
+
+    bool isEmpty() const {
+        return size_ == 0;
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+private:
+    DynamicStack* top_;
+    size_t size_;
+};
